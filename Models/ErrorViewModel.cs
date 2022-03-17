2b5e51163d6bead62857f057e18ecc1c3d0b2935
devalchawda0708 <deval.chawda@enate.net>
2022-03-17 15:28:44 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Enate_ChatBOT_POC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
